8e3e011a0d09d1fc1a1d5ae7f6e470dea79db18a
Eliaquín Encarnación <eliaquin@outlook.com>
2019-05-06 22:14:02 -0400

    Inicio formulario

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import Navigation from './components/common/Navigation';
 import Clients from './components/clients/Clients';
 import AddClient from './components/clients/AddClient';
-import Vehicles from './components/Vehicles';
+import Vehicles from './components/vehicles/Vehicles';
+import AddVehicle from './components/vehicles/AddVehicle';
 import Home from './components/Home.jsx';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
@@ -13,6 +14,7 @@ function App() {
       <Navigation />
       <main className="container mt-3">
         <Switch>
+          <Route path="/vehicles/add" component={AddVehicle} />
           <Route path="/vehicles" component={Vehicles} />
           <Route path="/clients/add" component={AddClient} />
           <Route path="/clients" component={Clients} />
